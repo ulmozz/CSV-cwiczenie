a2c145527e60d00310993682c744fdeaed1e7a53
Dariusz Ulma <ulmozz@gmail.com>
2025-06-27 16:27:22 +0200

    Stworzony pierwszy test CSV i cypress-config.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,30 @@
 const { defineConfig } = require("cypress");
 
+const fs = require('fs')
+const path = require('path')
+const neatCSV = require('neat-csv') // NOTE This require net-csv version older than 7.0.0.
+
+// NOTE This function is called when a project is opened or re-opened (e.g. due to the project's config changing).
 module.exports = defineConfig({
+  fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    supportFile: false,
+
+    // NOTE `on` is used to hook into various events Cypress emits. `config` is the resolved Cypress config.
+    async setupNodeEvents (on, config) {
+      const filename = path.join(__dirname, 'cypress/fixtures/users.csv')
+      console.log('loading file', filename)
+
+      const text = fs.readFileSync(filename, 'utf8')
+
+      const csv = await neatCSV(text)
+
+      console.log(csv)
+
+      // NOTE The data are available via Cypress.env("usersList") before the start of the tests.
+      config.env.usersList = csv
+
+      return config
     },
   },
 });
--- cypress/e2e/readCSV.cy.js
+++ cypress/e2e/readCSV.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+// we can access the Cypress.env() object outside the test:
+const csvUsers = Cypress.env('usersList')
+console.log(csvUsers)
+
+describe('Simple test on first row from CSV', () => {
+  it('has access to first row', () => {
+    cy.wrap(csvUsers)
+      .should('have.length', 3)
+      .then((rows) => {
+        const firstRow = rows[0]
+        console.log(csvUsers)
+        console.log(rows)
+        console.log(`The user in the first row has name: ${firstRow["first name"]}`)
+      })
   })
 })
