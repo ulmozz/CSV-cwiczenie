f767ae65c1b8ddf6dfd79205d3bb3ce2328a6220
Dariusz Ulma <ulmozz@gmail.com>
2025-06-30 15:33:47 +0200

    Dodano dynamiczne testy na pliku CSV

--- cypress/e2e/readCSV.cy.js
+++ cypress/e2e/readCSV.cy.js
@@ -16,3 +16,31 @@ describe('Simple test on first row from CSV', () => {
       })
   })
 })
+
+describe('Simple test on all rows from CSV', () => {
+  it('has access to all rows', () => {
+    cy.wrap(csvUsers)
+    .each((item, index) => {
+      cy.wrap(item).then((row) => {
+        cy.log(`The user ${index + 1} in the row has name: ${row["first name"]}, surname: ${row["last name"]} and ID: ${row["user id"]}`)
+      })
+    })
+  })
+})
+
+describe ('Users from CSV - dynamic tests', () => {
+  beforeEach(() => {
+    cy.visit('index.html')
+  })
+
+  csvUsers.forEach((taxpayer) => {
+    it (`has the user ${taxpayer['first name']} ${taxpayer['last name']}`, () => {
+      cy.contains('td[data-test-id=userId]', taxpayer['user id'])
+      .parent('tr')
+      .within(() => {
+        cy.contains('td[data-test-id=firstName]', taxpayer['first name'])
+        cy.contains('td[data-test-id=lastName]', taxpayer['last name'])
+      })
+    })
+  })
+})
